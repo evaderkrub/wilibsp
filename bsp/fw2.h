5f87338b10976b7076e4264ead49c2a73c4af308
Dave Robins <daverobins@intrepidcs.com>
2026-07-01 14:05:20 -0400

    feat(bsp): ST7796 480x320 display driver (DMA async flush + 5x7 font)

--- bsp/fw2.h
+++ bsp/fw2.h
@@ -6,7 +6,7 @@
 #define FW2_H
 
 #include "platform/board.h"   // (Task 3)
-// #include "display/st7796.h" // (Task 4)
+#include "display/st7796.h" // (Task 4)
 // #include "input/ft6336.h"   // (Task 5)
 // #include "leds/ws2812_driver.h" // (Task 6)
 
